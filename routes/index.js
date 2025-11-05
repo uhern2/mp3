b695fe3f47a339f4d59ba28c7835a2f1c3d92080
uhern2 <uhern2@illinois.edu>
2025-11-04 22:09:31 -0600

    tweaked code to allow me to test dbFill.py

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@
  * Connect all of your endpoints together here.
  */
 module.exports = function (app, router) {
-    app.use('/api', require('./home.js')(router));
+    const routes = require('./home.js')(router);
+    app.use('/api', routes);
+    app.use('/', routes); 
 };
